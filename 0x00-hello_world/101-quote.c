0f7886c1dedf3a3cdb5661a705ae9c3b03b37d45
HettyD <hettydagogz93@gmail.com>
2022-10-13 03:37:05 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+* main - print to standard error
+* Description: print texts to standard error instead of stdout
+* Return: 0 after running
+*/
+int main(void)
+{
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, text, 59);
+
+	return (1);
+}
