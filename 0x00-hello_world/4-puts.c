4f4fff60c28b07644dc7d9566b6d4c5c95badd62
HettyD <hettydagogz93@gmail.com>
2022-10-13 03:05:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,27 @@
 #include<stdio.h>
-
 /**
- *
- * main - Printing
- * function - main
- *Description: this just prints a line of text i put there
- * Return: the return value should be zero
- */
+
+* main - this prints out text
+
+* function - main
+
+*Description: this just prints a line of text i put there
+
+* Return: the return value should be zero
+*/
+
 int main(void) /* return 0 */
+
 {
-	
-         	char Mytext[] = "\"Programming is like building a multilingual puzzle";
 
-		puts(Mytext);
+	char Mytext[] = "\"Programming is like building a multilingual puzzle";
 
-		return (0);
+
+
+	puts(Mytext);
+
+
+
+	return (0);
 }
+
