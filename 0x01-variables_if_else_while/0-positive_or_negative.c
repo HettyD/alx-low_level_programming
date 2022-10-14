5e2d82656ce10c385edbea4d224d7ceae54ab9ee
HettyD <hettydagogz93@gmail.com>
2022-10-14 07:10:39 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /* more headers goes there */
 /**
 * main - checks negative or positive of random numbers
-* Description: generate random numbers
+* Description: generate random numbers and tells us it's sign
 * Return: it returns 0 after executing
 */
 int main(void)
@@ -19,20 +19,16 @@ int main(void)
 if (n > 0)
 {
 	printf("%d is positive\n", n);
-
 }
 else if (n == 0)
 {
 	printf("%d is zero\n", n);
-
 }
 else
 {
 	printf("%d is negative\n", n);
-
 }
 	/* your code goes there */
 
 	return (0);
-
 }
