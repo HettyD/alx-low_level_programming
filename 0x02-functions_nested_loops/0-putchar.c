f01c2c6f520c19893e6f75d9abd30f9495b36592
HettyD <hettydagogz93@gmail.com>
2022-10-18 05:23:08 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
 #include <unistd.h>
 #include "main.h"
 
-/**
-* main - putchar without putchar or printf or puts
-* Return: 0 if error free
+/** main - print putchar
+* Return: 0 always
 */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
 	return (0);
 
