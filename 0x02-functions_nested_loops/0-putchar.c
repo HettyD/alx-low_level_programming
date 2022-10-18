6e7676e3f0347fe2efe0bf2b2dfe6446d62bf921
HettyD <hettydagogz93@gmail.com>
2022-10-18 05:01:06 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,29 +1,23 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
-* main - a program that prints _putchar and moves to a new line
-* Return: 0 if executed
+* main - putchar without putchar or printf or puts
+* Return: 0 if error free
 */
+
 int main(void)
 {
 	_putchar('_');
-
 	_putchar('p');
-
 	_putchar('u');
-	
 	_putchar('t');
-	
 	_putchar('c');
-	
 	_putchar('h');
-	
 	_putchar('a');
-	
 	_putchar('r');
-	
 	_putchar('\n');
 
 	return (0);
+
 }
+
